8b3615db0cfcb3d0ce9e76cd6bc528f497409d64
jjones <jimmy.jones@tutanota.com>
2023-11-27 18:31:18 -0700

    add function to engineer features for a data frame

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -44,3 +44,42 @@ df["Stage1.Output.Measurement0.U.Actual"] = cleaned_series
 
 df["Stage1.Output.Measurement0.U.Actual"].plot()
 df = df.iloc[:, :42]
+
+
+def engineer_features(df, lag_features, window_size=10):
+    """
+    Engineers features for a DataFrame by creating lag features and rolling statistics.
+
+    Parameters:
+    df (pandas.DataFrame): DataFrame to engineer features for.
+    lag_features (list): List of column names to create lag features for.
+    window_size (int): Window size for rolling calculations.
+
+    Returns:
+    pandas.DataFrame: DataFrame with engineered features.
+    """
+    df_eng = df.copy()
+
+    for col in lag_features:
+        if col != "Stage1.Output.Measurement0.U.Actual":
+            for i in range(1, window_size + 1):
+                df_eng[f"{col}_lag{i}"] = df[col].shift(i)
+
+    for col in lag_features:
+        if col != "Stage1.Output.Measurement0.U.Actual":
+            df_eng[f"{col}_rolling_mean"] = df[col].rolling(window=window_size).mean()
+            df_eng[f"{col}_rolling_std"] = df[col].rolling(window=window_size).std()
+            df_eng[f"{col}_rolling_min"] = df[col].rolling(window=window_size).min()
+            df_eng[f"{col}_rolling_max"] = df[col].rolling(window=window_size).max()
+
+    df_eng = df_eng.dropna()
+
+    return df_eng
+
+
+lag_features = df.columns.tolist()
+window_size = 60
+
+df_eng = engineer_features(df, lag_features, window_size)
+
+df_eng.to_pickle("../../data/interim/data_engineered.pkl")
